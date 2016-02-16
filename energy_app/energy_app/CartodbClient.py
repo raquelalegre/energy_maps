31050af68469bd262866474f198d187f7b3eef3b
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-02-16 21:08:55 +0000

    Add PostGIS query to filter tweets by area.

--- energy_app/energy_app/CartodbClient.py
+++ energy_app/energy_app/CartodbClient.py
@@ -44,3 +44,14 @@ class CartodbClient:
         sql_where = "WHERE body LIKE '%{0}%'".format(company)
         tweets = self.get_tweets(sql_where)
         return tweets
+
+    def get_tweets_by_area(self, area):
+        #Use postgres to filter by area
+        sql_query = """
+        SELECT * FROM
+            (SELECT * FROM energy_tweets) AS et,
+            (SELECT the_geom_webmercator AS gwm
+                FROM {0}) AS shp
+        WHERE ST_WITHIN(et.the_geom_webmercator, shp.gwm)""".format(area)
+        results = self.api.sql(sql_query)
+        return results['rows']
