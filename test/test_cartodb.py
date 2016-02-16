28e64d39d58fec0146aa6994631986f76ccd4376
Raquel Alegre <r.alegre@ucl.ac.uk>
2016-02-16 00:28:04 +0000

    Added some tests for CartoDB connection.

---
+++ test/test_cartodb.py
@@ -0,0 +1,12 @@
+import pytest
+from CartodbClient import CartodbClient
+
+class TestCartoDB:
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def test_cartodb_connection(self):
+        client = CartodbClient()
